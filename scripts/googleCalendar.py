c273b1337f50b2f47121f71c22b92d6ed32ddf64
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2025-08-23 16:54:58 -0600

    Basic file structure

---
+++ scripts/googleCalendar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from src.classes.calendarHandler import calendarHandler
+from src.functions.calendarUtils import calendarSetUp
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
