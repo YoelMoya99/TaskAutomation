9c05272f2d66dcb8e396cbf1d6ab2ea0321a86da
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2025-08-23 18:30:58 -0600

    Can read events

--- scripts/googleCalendar.py
+++ scripts/googleCalendar.py
@@ -2,10 +2,39 @@
 
 from src.classes.calendarHandler import calendarHandler
 from src.functions.calendarUtils import calendarSetUp
+import datetime
+import pytz
 
 def main() -> None:
-    pass
+    
+    orquestrator = calendarHandler()
+    orquestrator.calendarObj = calendarSetUp()
+    orquestrator.getCalendars()
+   
 
+    '''
+    START TESTING FUNCTIONALITY OF THE CALENDAR
+
+    for calendar in orquestrator.calendars:
+        calendar_id = calendar['id']
+        print(f"\n--- Events in calendar: {calendar.get('summary', calendar_id)} ---")
+        events_result = orquestrator.calendarObj.events().list(
+            calendarId=calendar_id,
+            timeMin=orquestrator.startTime,
+            timeMax=orquestrator.endTime,
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
+        
+        events = events_result.get('items', [])
+        if not events:
+            print("No events today")
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event.get('summary', 'No title'))
+
+    END TESTING FUNCTIONALITY OF THE CALENDAR
+    '''
 
 if __name__ == '__main__':
     main()
